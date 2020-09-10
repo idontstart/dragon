aaada7ba9791d0b8db5e4b60298fae629f14f47e
idontstart <69805745+idontstart@users.noreply.github.com>
2020-09-10 10:35:48 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,8 +4,49 @@ Creator: Elijah S
 Description: Flappy Bird clone with a dragon.
 """
 #setup
+info.set_life(3)
+info.set_score(0)
+#TODO add backgroud
+#TODO add lava/fire
 
 #make dragon 
+Dragon = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+"""))
+Dragon.x = 40
+Dragon.ay = 150
+
+def on_flap():
+    Dragon.vy = -100
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_flap)
+
+def on_update():
+    y = Dragon.y
+    if y > scene.screen_height():
+        die()
+    elif y < 0:
+        Dragon.y = 0
+game.on_update(on_update)
+
+def die():
+    info.change_life_by(-1)
+    Dragon.y = scene.screen_height()/2
+    Dragon.vy = 0
 
 #make chains
 
--- main.ts
+++ main.ts
@@ -4,3 +4,47 @@ Creator: Elijah S
 Description: Flappy Bird clone with a dragon.
 
  */
+// setup
+info.setLife(3)
+info.setScore(0)
+// TODO add backgroud
+// TODO add lava/fire
+// make dragon 
+let Dragon = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`)
+Dragon.x = 40
+Dragon.ay = 150
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_flap() {
+    Dragon.vy = -100
+})
+game.onUpdate(function on_update() {
+    let y = Dragon.y
+    if (y > scene.screenHeight()) {
+        die()
+    } else if (y < 0) {
+        Dragon.y = 0
+    }
+    
+})
+function die() {
+    info.changeLifeBy(-1)
+    Dragon.y = scene.screenHeight() / 2
+    Dragon.vy = 0
+}
+
