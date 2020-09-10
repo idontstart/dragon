eb2fcf03edd5d3a3a22d556b1d7e9fff21df495f
idontstart <69805745+idontstart@users.noreply.github.com>
2020-09-10 10:01:46 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,12 @@
+""" 
+Title: Dragon Flap
+Creator: Elijah S
+Description: Flappy Bird clone with a dragon.
+"""
+#setup
+
+#make dragon 
+
+#make chains
+
+#detect collision
---
+++ main.ts
@@ -0,0 +1,6 @@
+/**  
+Title: Dragon Flap
+Creator: Elijah S
+Description: Flappy Bird clone with a dragon.
+
+ */
